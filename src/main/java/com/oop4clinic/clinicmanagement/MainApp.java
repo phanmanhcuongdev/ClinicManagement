5587f50bb92bc3bb7d274929b567385255302659
Vie <danghavy1234@gmail.com>
2025-09-23 19:48:11 +0700

    Giao dien dang nhap va menu cua tung vai tro

--- src/main/java/com/oop4clinic/clinicmanagement/MainApp.java
+++ src/main/java/com/oop4clinic/clinicmanagement/MainApp.java
@@ -7,18 +7,23 @@ import javafx.stage.Stage;
 
 public class MainApp extends Application {
     @Override
-    public void start(Stage stage) throws Exception
-    {
+    public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader(
-                getClass().getResource("/com/oop4clinic/clinicmanagement/fxml/Splash.fxml")
+                getClass().getResource("/com/oop4clinic/clinicmanagement/fxml/Login.fxml")
+                // getClass().getResource("/com/oop4clinic/clinicmanagement/fxml/MenuPatient.fxml")
+                // getClass().getResource("/com/oop4clinic/clinicmanagement/fxml/MenuAdmin.fxml")
+                // getClass().getResource("/com/oop4clinic/clinicmanagement/fxml/MenuDoctor.fxml")
         );
         Scene scene = new Scene(loader.load());
         stage.setScene(scene);
-        stage.setTitle("OOP4Clinic");
+        stage.setTitle("Clinic OOP4");
         stage.show();
     }
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         launch(args);
     }
 }
+
+
+
